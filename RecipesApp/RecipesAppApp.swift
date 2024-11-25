3feeac04b02f26fc8e908035560d8c57a87be9df
Kulbir  Sidhu <kskulbirsidhu@gmail.com>
2024-11-25 09:26:57 -0600

    first project commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Ramandeep Singh on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
